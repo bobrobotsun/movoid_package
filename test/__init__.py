485f00570a48742b928da004c347bfe7f0a8f92d
bobrobotsun <bobrobotsun@163.com>
2024-08-22 01:28:15 +0800

    2024.08.21.01-movoid dev 可以触发github test

---
+++ test/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/3/2 20:58
+# Description   :
+"""
---
+++ test/test_stub.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : test_stub
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/8/22 0:46
+# Description   : 
+"""
+from movoid_package import Stub
+
+
+class TestStub:
+    def test_stub(self):
+        stub = Stub('test/temp1.py')
