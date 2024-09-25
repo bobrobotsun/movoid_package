e2e3cd4f53caa540efd2cb01a952ef3efc245fdf
bobrobotsun <bobrobotsun@163.com>
2024-09-25 23:31:46 +0800

    2024.09.25-movoid 增加了一些装饰逻辑

--- movoid_package/package.py
+++ movoid_package/package.py
@@ -6,22 +6,15 @@
 # Time          : 2024/9/17 16:37
 # Description   : 
 """
-import importlib
-import pathlib
+from .for_import import _get_root_path, python_path, import_module
 
 
 class Package:
-    @classmethod
-    def init(cls, root_path):
-        temp_pathlib = pathlib.Path(root_path)
-        if temp_pathlib.is_file():
-            temp_pathlib = temp_pathlib.parent
-        cls.root_path = temp_pathlib
+    def __init__(self, root_path):
+        self.root_path = _get_root_path(root_path)
+        python_path(self.root_path)
 
-    init(__file__)
-
-    @classmethod
-    def decorate_python(cls, package_name, object_name, decorator, args=None, kwargs=None, has_args=None):
+    def decorate_python(self, package_name, object_name, decorator, args=None, kwargs=None, has_args=None):
         """
         用python的方法对某个包内的元素进行装饰器
         :param package_name: 包名，str，从root的包路径
@@ -32,7 +25,7 @@ class Package:
         :param has_args: 装饰器是否存在参数
         :return:
         """
-        package = importlib.import_module(package_name)
+        package = import_module(package_name)
         ori_object = getattr(package, object_name)
         if has_args is None:
             if args is None and kwargs is None:
@@ -49,9 +42,35 @@ class Package:
             now_object = decorator(ori_object)
         setattr(package, object_name, now_object)
 
-    @classmethod
-    def import_package(cls, package_name: str):
-        if package_name.startswith('.'):
-            pass
+    def analyse_dict(self, ori_dict: dict) -> dict:
+        if 'type' in ori_dict:
+            _type = ori_dict['type']
+            if _type == 'import':
+                re_value = import_module(ori_dict['package'], ori_dict.get('object', None))
+            else:
+                re_value = None
         else:
-            return importlib.import_module(package_name)
+            re_value = ori_dict
+            for k, v in ori_dict.items():
+                if isinstance(v, list):
+                    re_value[k] = self.analyse_list(v)
+                elif isinstance(v, dict):
+                    re_value[k] = self.analyse_dict(v)
+                else:
+                    re_value[k] = v
+        return re_value
+
+    def analyse_list(self, ori_list: list) -> list:
+        re_value = []
+        for i, v in enumerate(ori_list):
+            if isinstance(v, list):
+                re_value.append(self.analyse_list(v))
+            elif isinstance(v, dict):
+                re_value.append(self.analyse_dict(v))
+            else:
+                re_value.append(v)
+        return re_value
+
+    def decorate_dict(self, tar_dict: dict):
+        tar_dict = self.analyse_dict(tar_dict)
+        self.decorate_python(**tar_dict)
