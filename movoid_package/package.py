d9ac51564d05f61e393ece5575ce9b897e06043b
bobrobotsun <42180642+bobrobotsun@users.noreply.github.com>
2024-10-19 22:00:28 +0800

    2014.10.19
    修复了传递函数会导致解析目标错误的BUG
    删除了抽象成文件的部分
    新增了直接元素替换部分
    现在可以导入get_root_path，来寻找当前的根目录

--- movoid_package/package.py
+++ movoid_package/package.py
@@ -6,17 +6,18 @@
 # Time          : 2024/9/17 16:37
 # Description   : 
 """
-from .for_import import _get_root_path, python_path, import_module
+from .for_import import get_root_path, python_path, import_module
 
 
 class Package:
     def __init__(self, root_path=None):
-        self.root_path = _get_root_path(root_path)
+        self.root_path = get_root_path(root_path, 2)
         python_path(self.root_path)
+        self.old = {}
 
-    def decorate_python(self, package_name, object_name, decorator, args=None, kwargs=None, has_args=None):
+    def decorate_replace(self, package_name, object_name, decorator, args=None, kwargs=None, has_args=None):
         """
-        用python的方法对某个包内的元素进行装饰器
+        对某个包内的元素进行装饰器
         :param package_name: 包名，str，从root的包路径
         :param object_name: 目标元素的名称，str，目标元素的类型可以是函数，也可以是类，但是需要自己对应好
         :param decorator: 装饰器，传元素本体
@@ -27,6 +28,8 @@ class Package:
         """
         package = import_module(package_name)
         ori_object = getattr(package, object_name)
+        self.old.setdefault((package.__name__, object_name), [])
+        self.old[(package.__name__, object_name)].append(ori_object)
         if has_args is None:
             if args is None and kwargs is None:
                 has_args = False
@@ -42,35 +45,16 @@ class Package:
             now_object = decorator(ori_object)
         setattr(package, object_name, now_object)
 
-    def analyse_dict(self, ori_dict: dict) -> dict:
-        if 'type' in ori_dict:
-            _type = ori_dict['type']
-            if _type == 'import':
-                re_value = import_module(ori_dict['package'], ori_dict.get('object', None))
-            else:
-                re_value = None
-        else:
-            re_value = ori_dict
-            for k, v in ori_dict.items():
-                if isinstance(v, list):
-                    re_value[k] = self.analyse_list(v)
-                elif isinstance(v, dict):
-                    re_value[k] = self.analyse_dict(v)
-                else:
-                    re_value[k] = v
-        return re_value
-
-    def analyse_list(self, ori_list: list) -> list:
-        re_value = []
-        for i, v in enumerate(ori_list):
-            if isinstance(v, list):
-                re_value.append(self.analyse_list(v))
-            elif isinstance(v, dict):
-                re_value.append(self.analyse_dict(v))
-            else:
-                re_value.append(v)
-        return re_value
-
-    def decorate_dict(self, tar_dict: dict):
-        tar_dict = self.analyse_dict(tar_dict)
-        self.decorate_python(**tar_dict)
+    def replace_object(self, package_name, object_name, new_object):
+        """
+        直接将某个元素替代为另一个元素
+        :param package_name:
+        :param object_name:
+        :param new_object:
+        :return:
+        """
+        package = import_module(package_name)
+        ori_object = getattr(package, object_name)
+        self.old.setdefault((package.__name__, object_name), [])
+        self.old[(package.__name__, object_name)].append(ori_object)
+        setattr(package, object_name, new_object)
