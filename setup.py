e854f50ce569b74a519fced8bc99452d40258267
bobrobotsun <bobrobotsun@163.com>
2024-10-28 12:36:17 +0800

    1.3.3更新 修复了一个Stub无法正确识别本地类的错误

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_package',
-    version='1.3.2',
+    version='1.3.3',
     packages=find_packages(),
     url='',
     license='',
